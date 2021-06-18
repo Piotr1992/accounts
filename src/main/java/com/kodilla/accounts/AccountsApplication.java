1157b745da4bf395bf3b9121c4900aaad62af230
Lenovo <szyperek.piotr@o2.pl>
2021-06-19 01:32:49 +0200

    1.7 Spring Cloud Contract - Generowanie stubów ver. 2

--- src/main/java/com/kodilla/accounts/AccountsApplication.java
+++ src/main/java/com/kodilla/accounts/AccountsApplication.java
@@ -2,11 +2,11 @@ package com.kodilla.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+//import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableCircuitBreaker
+//@EnableCircuitBreaker
 //@EnableFeignClients
 @SpringBootApplication
 @EnableDiscoveryClient
